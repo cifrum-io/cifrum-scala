93efe9c2c73c62c1fd8d0c85e0b4ebb6e450da94
Alexander Myltsev <alexander@myltsev.com>
2019-09-21 08:26:32 +0300

    Implement `VectorEodSeries.fromCSV`

--- core/src/main/scala/io/okama/timeseries/VectorEodSeries.scala
+++ core/src/main/scala/io/okama/timeseries/VectorEodSeries.scala
@@ -4,6 +4,8 @@ package timeseries
 import unit._
 
 import org.joda.time.{convert => _, _}
+import com.github.tototoshi.csv._
+import scala.io.Source
 
 type VectorEodDataType = Vector[(LocalDate, Double)]
 
@@ -35,4 +37,17 @@ class VectorEodSeries(data: VectorEodDataType) extends TimeSeries {
       case Vector() => "VectorEodSeries()"
       case v        => s"VectorEodSeries(${v.head} .. ${v.last})"
     }
+}
+
+object VectorEodSeries {
+  def fromCsv(url: String, dateColumn: String, valueColumn: String): VectorEodSeries = {
+    val items = CSVReader.open(Source.fromURL(url))(new TSVFormat{}).allWithHeaders
+    val data =
+      for (item <- items) yield {
+        val d = LocalDate.parse(item(dateColumn))
+        val v = item(valueColumn).toDouble
+        (d, v)
+      }
+    VectorEodSeries(data.toVector)
+  }
 }
