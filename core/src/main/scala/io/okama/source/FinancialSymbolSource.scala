a06a45b654ce6c33e758a03c040530437501f9e0
Alexander Myltsev <alexander@myltsev.com>
2019-09-25 21:34:38 +0300

    Minor fix: rename argument

--- core/src/main/scala/io/okama/source/FinancialSymbolSource.scala
+++ core/src/main/scala/io/okama/source/FinancialSymbolSource.scala
@@ -4,6 +4,6 @@ package source
 import unit._
 import timeseries._
 
-abstract class FinancialSymbolsSource(val namespace: String) {
-  def getFinancialSymbol(name: String): Option[FinancialSymbol]
+abstract class FinancialSymbolsSource[T <: PeriodFrequency, TS <: TimeSeries[T, Double], FT <: FinancialSymbol[T, TS]](val namespace: String) {
+  def getFinancialSymbol(code: String): Option[FT]
 }
