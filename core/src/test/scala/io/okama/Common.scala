9bc9e05018b744b79d5c80e63ee2fb52d0afb355
Alexander Myltsev <alexander@myltsev.com>
2019-09-29 11:49:31 +0300

    Move `registry` to Common trait

--- core/src/test/scala/io/okama/Common.scala
+++ core/src/test/scala/io/okama/Common.scala
@@ -6,4 +6,5 @@ class Module extends AbstractModule
 
 trait Common {
   val injector = Guice.createInjector(Module())
+  val registry = injector.getInstance(classOf[portfolio.Registry])
 }
--- core/src/test/scala/io/okama/portfolio/RegistryIntegrationTest.scala
+++ core/src/test/scala/io/okama/portfolio/RegistryIntegrationTest.scala
@@ -1,13 +1,11 @@
 package io.okama
 package portfolio
 
-import com.google.inject.{Guice, AbstractModule}
 import portfolio.{AssetNamespace => ANS}
 import verify._
 
 object RegistryIntegrationTest extends BasicTestSuite with Common {
   test("Registry.get") {
-    val registry = injector.getInstance(classOf[portfolio.Registry])
     assert((registry.get(ANS.micex, "SBER"): Option[AssetMICEX]).isDefined)
     assert((registry.get(ANS.us, "AAPL"): Option[AssetUS]).isDefined)
     assert((registry.get(ANS.us, "absent"): Option[AssetUS]).isEmpty)
